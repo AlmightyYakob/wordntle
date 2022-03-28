00715ed0b9fd77eaeb9aa5361eb5d5f463ec1a0a
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-03-28 18:59:20 -0400

    vuetify

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <HelloWorld/> -->
+      <Game />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Game from './Game.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Game,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
-import Game from './Game.vue'
-import './game.css'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-// resize for scaling the board size
-window.addEventListener('resize', onResize)
-// set size on startup
-onResize()
+loadFonts()
 
-function onResize() {
-  // get actual vh on mobile
-  document.body.style.setProperty('--vh', window.innerHeight + 'px')
-}
-
-createApp(Game).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
