eb1c6f9d56775cec6c822287a7671cf5b8ea4efb
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-03-28 18:44:32 -0400

    WIP update game

---
+++ src/game.ts
@@ -0,0 +1,169 @@
+import { onUnmounted, ref } from 'vue'
+import { getWordOfTheDay, invalidGuesses } from './words'
+import { LetterState } from './types'
+
+// Get word of the day
+const answer = getWordOfTheDay()
+
+// Storage state
+const infoMenuOpen = ref(true);
+// TODO: Fetch storage and set to false if found
+
+// Board state. Each tile is represented as { letter, state }
+export const board = $ref(
+  Array.from({ length: 6 }, () =>
+    Array.from({ length: 5 }, () => ({
+      letter: '',
+      state: LetterState.INITIAL
+    }))
+  )
+)
+
+// Current active row.
+let currentRowIndex = $ref(0)
+const currentRow = $computed(() => board[currentRowIndex])
+
+// Feedback state: message and shake
+export let message = $ref('')
+export let grid = $ref('')
+export let shakeRowIndex = $ref(-1)
+export let success = $ref(false)
+
+// Keep track of revealed letters for the virtual keyboard
+export const letterStates: Record<string, LetterState> = $ref({})
+
+// Handle keyboard input.
+let allowInput = true
+
+export const onKeyup = (e: KeyboardEvent) => onKey(e.key)
+
+window.addEventListener('keyup', onKeyup)
+
+export function onKey(key: string) {
+  if (!allowInput) return
+  if (/^[a-zA-Z]$/.test(key)) {
+    fillTile(key.toLowerCase())
+  } else if (key === 'Backspace') {
+    clearTile()
+  } else if (key === 'Enter') {
+    completeRow()
+  }
+}
+
+function fillTile(letter: string) {
+  for (const tile of currentRow) {
+    if (!tile.letter) {
+      tile.letter = letter
+      break
+    }
+  }
+}
+
+function clearTile() {
+  for (const tile of [...currentRow].reverse()) {
+    if (tile.letter) {
+      tile.letter = ''
+      break
+    }
+  }
+}
+
+function completeRow() {
+  if (currentRow.every((tile) => tile.letter)) {
+    const guess = currentRow.map((tile) => tile.letter).join('')
+    if (guess !== answer && invalidGuesses.includes(guess)) {
+      shake()
+      showMessage(`That's a real word`)
+      return
+    }
+
+    const answerLetters: (string | null)[] = answer.split('')
+    // first pass: mark correct ones
+    currentRow.forEach((tile, i) => {
+      if (answerLetters[i] === tile.letter) {
+        tile.state = letterStates[tile.letter] = LetterState.CORRECT
+        answerLetters[i] = null
+      }
+    })
+    // second pass: mark the present
+    currentRow.forEach((tile) => {
+      if (!tile.state && answerLetters.includes(tile.letter)) {
+        tile.state = LetterState.PRESENT
+        answerLetters[answerLetters.indexOf(tile.letter)] = null
+        if (!letterStates[tile.letter]) {
+          letterStates[tile.letter] = LetterState.PRESENT
+        }
+      }
+    })
+    // 3rd pass: mark absent
+    currentRow.forEach((tile) => {
+      if (!tile.state) {
+        tile.state = LetterState.ABSENT
+        if (!letterStates[tile.letter]) {
+          letterStates[tile.letter] = LetterState.ABSENT
+        }
+      }
+    })
+
+    allowInput = false
+    if (currentRow.every((tile) => tile.state === LetterState.CORRECT)) {
+      // yay!
+      setTimeout(() => {
+        grid = genResultGrid()
+        showMessage(
+          ['Genius', 'Magnificent', 'Impressive', 'Splendid', 'Great', 'Phew'][
+            currentRowIndex
+          ],
+          -1
+        )
+        success = true
+      }, 1600)
+    } else if (currentRowIndex < board.length - 1) {
+      // go the next row
+      currentRowIndex++
+      setTimeout(() => {
+        allowInput = true
+      }, 1600)
+    } else {
+      // game over :(
+      setTimeout(() => {
+        showMessage(answer.toUpperCase(), -1)
+      }, 1600)
+    }
+  } else {
+    shake()
+    showMessage('Not enough letters')
+  }
+}
+
+function showMessage(msg: string, time = 1000) {
+  message = msg
+  if (time > 0) {
+    setTimeout(() => {
+      message = ''
+    }, time)
+  }
+}
+
+function shake() {
+  shakeRowIndex = currentRowIndex
+  setTimeout(() => {
+    shakeRowIndex = -1
+  }, 1000)
+}
+
+const icons = {
+  [LetterState.CORRECT]: '🟩',
+  [LetterState.PRESENT]: '🟨',
+  [LetterState.ABSENT]: '⬜',
+  [LetterState.INITIAL]: null
+}
+
+function genResultGrid() {
+  return board
+    .slice(0, currentRowIndex + 1)
+    .map((row) => {
+      return row.map((tile) => icons[tile.state]).join('')
+    })
+    .join('\n')
+}
